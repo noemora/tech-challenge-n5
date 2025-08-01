c5ce0ece7c201cce0d9318c760fd4aee751cad1f
Noé Mora <noedev89@gmail.com>
2025-07-31 20:30:58 -0400

    fix dependences on host to make MFE2 work

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,15 +1,13 @@
 import './App.css';
 import RemoteButton from 'remote/Button';
-import RemoteButton2 from 'remote2/Button';
+import RemoteApp2 from 'remote2/App';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Host App</h1>
       <RemoteButton />
-      <p>This button is loaded from the remote application.</p>
-      <RemoteButton2 />
-      <p>This button is loaded from the remote2 application.</p>
+      <RemoteApp2 />
     </div>
   );
 }
